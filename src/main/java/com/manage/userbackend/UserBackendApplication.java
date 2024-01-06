19de2af20032431709b3a1534761aff41c7b7d2b
Ganeesha <yasinthav@ts.lk>
2024-01-06 10:38:52 +0530

    First Commit

---
+++ src/main/java/com/manage/userbackend/UserBackendApplication.java
@@ -0,0 +1,13 @@
+package com.manage.userbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/manage/userbackend/enums/Roles.java
@@ -0,0 +1,6 @@
+package com.manage.userbackend.enums;
+
+public enum Roles {
+    USER,
+    ADMIN
+}
